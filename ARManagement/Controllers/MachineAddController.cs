fce8f3bee169cac97dd6e26e70118ce6673f5821
Portfolio Project <134990681+peter85246@users.noreply.github.com>
2024-05-07 01:05:43 +0800

    Add files via upload

--- ARManagement/Controllers/MachineAddController.cs
+++ ARManagement/Controllers/MachineAddController.cs
@@ -46,23 +46,38 @@ namespace ARManagement.Controllers
                 //}
                 #endregion
 
+                //var sql = $@"
+                //            SELECT 
+                //                m.*,
+                //                md.""MachineDeviceId""
+                //            FROM public.""MachineAdd"" m
+                //            JOIN public.""MachineDevice"" md ON m.""MachineAddId"" = md.""MachineAddId"" AND md.""Deleted"" = 0
+                //            WHERE m.""CompanyId"" = @CompanyId AND m.""Deleted"" = 0
+                //        ";
+
                 var sql = $@"
                             SELECT 
-                                m.*,
-                                md.""MachineDeviceId""
+                                m.""MachineAddId"", m.""MachineType"", m.""ModelSeries"", m.""MachineName""
                             FROM public.""MachineAdd"" m
-                            JOIN public.""MachineDevice"" md ON m.""MachineAddId"" = md.""MachineAddId"" AND md.""Deleted"" = 0
-                            WHERE m.""CompanyId"" = @CompanyId AND m.""Deleted"" = 0
-                        ";
+                            WHERE m.""CompanyId"" = @CompanyId AND m.""Deleted"" = 0";
+
+
 
                 if (!string.IsNullOrEmpty(post.Keyword))
                 {
-                    sql += $@" AND (""MachineType"" LIKE CONCAT('%', @Keyword, '%') OR ""ModelSeries"" LIKE CONCAT('%', @Keyword, '%') OR ""MachineModel"" LIKE CONCAT('%', @Keyword, '%'))";
+                    sql += $@" AND (""MachineType"" LIKE CONCAT('%', @Keyword, '%') OR ""ModelSeries"" LIKE CONCAT('%', @Keyword, '%') OR ""MachineName"" LIKE CONCAT('%', @Keyword, '%'))";
                 }
 
                 sql += $@" ORDER BY m.""MachineAddId"" ASC";
 
                 List<MachineAddOverview> machineAddOverview = await _baseRepository.GetAllAsync<MachineAddOverview>(sql, new { Keyword = post.Keyword, CompanyId = CompanyId });
+                foreach (var item in machineAddOverview)
+                {
+                    if (!string.IsNullOrEmpty(item.MachineImage))
+                    {
+                        item.MachineImage = $"{baseURL}upload/machine/{item.MachineAddId}/{item.MachineImage}";
+                    }
+                }
 
                 apiResult.Code = "0000";
                 apiResult.Message = _responseCodeHelper.GetResponseCodeString(apiResult.Code);
@@ -101,7 +116,7 @@ namespace ARManagement.Controllers
 
                 #region 機台是否存在
                 string conditionSQL = $@"""MachineAddId"" = @MachineAddId";
-                var machineInfo = await _baseRepository.GetOneAsync<MachineAddInfo>("Machine", conditionSQL, new { MachineAddId = post.MachineAddId });
+                var machineInfo = await _baseRepository.GetOneAsync<MachineAddInfo>("MachineAdd", conditionSQL, new { MachineAddId = post.MachineAddId });
 
                 if (machineInfo == null)
                 {
@@ -117,6 +132,11 @@ namespace ARManagement.Controllers
                     return Ok(apiResult);
                 }
                 #endregion
+
+                if (!string.IsNullOrEmpty(machineInfo.MachineImage))
+                {
+                    machineInfo.MachineImage = $"{baseURL}upload/machineAdd/{machineInfo.MachineAddId}/{machineInfo.MachineImage}";
+                }
 
                 apiResult.Code = "0000";
                 apiResult.Message = _responseCodeHelper.GetResponseCodeString(apiResult.Code);
@@ -154,8 +174,7 @@ namespace ARManagement.Controllers
                 #endregion
 
                 #region 機台是否存在
-                string conditionSQL = $@"""MachineAddCode"" = @MachineAddCode";
-                var machineInfo = await _baseRepository.GetOneAsync<MachineAddInfo>("MachineAdd", conditionSQL, new { MachineAddCode = post.MachineAddCode });
+                var machineInfo = await _baseRepository.GetOneAsync<MachineAddInfo>("MachineAdd");
 
                 if (machineInfo == null)
                 {
@@ -215,7 +234,7 @@ namespace ARManagement.Controllers
                 #region 欄位驗證
                 //if (string.IsNullOrEmpty(post.MachineType) ||
                 //    string.IsNullOrEmpty(post.ModelSeries) ||
-                //    string.IsNullOrEmpty(post.MachineModel))
+                //    string.IsNullOrEmpty(post.MachineName))
                 //{
                 //    apiResult.Code = "2003"; //有必填欄位尚未填寫
                 //    apiResult.Message = _responseCodeHelper.GetResponseCodeString(apiResult.Code);
@@ -224,7 +243,7 @@ namespace ARManagement.Controllers
 
                 //if (post.MachineType.Length > 100 || 
                 //    post.ModelSeries.Length > 100 ||
-                //    post.MachineModel.Length > 100)
+                //    post.MachineName.Length > 100)
                 //{
                 //    apiResult.Code = "2005"; //輸入文字字數不符合長度規範
                 //    apiResult.Message = _responseCodeHelper.GetResponseCodeString(apiResult.Code);
@@ -258,14 +277,14 @@ namespace ARManagement.Controllers
 
                 //需檢查MachineAddCode有沒有重複
                 MachineAdd machineinfoRepeat = new MachineAdd();
-                var conditionSQL = $@"""Deleted"" = 0 AND ""CompanyId"" = @CompanyId AND ""MachineAddCode"" = @MachineAddCode";
+                var conditionSQL = $@"""Deleted"" = 0 AND ""CompanyId"" = @CompanyId";
 
                 if (post.MachineAddId != 0)
                 {
                     conditionSQL += " AND \"MachineAddId\" != @MachineAddId";
                 }
 
-                machineinfoRepeat = await _baseRepository.GetOneAsync<MachineAdd>("MachineAdd", conditionSQL, new { CompanyId = CompanyId, MachineAddCode = post.MachineAddCode, MachineAddId = post.MachineAddId });
+                machineinfoRepeat = await _baseRepository.GetOneAsync<MachineAdd>("MachineAdd", conditionSQL, new { CompanyId = CompanyId,  MachineAddId = post.MachineAddId });
 
                 if (machineinfoRepeat != null) //代表有重複
                 {
@@ -275,7 +294,23 @@ namespace ARManagement.Controllers
                     return Ok(apiResult);
                 }
 
-                //判斷machine資料夾是否存在
+                FolderFunction folderFunction = new FolderFunction();
+                string ImageName = string.Empty; //圖片路徑
+                if (post.MachineImageObj != null)
+                {
+                    var validFileNameEx = new List<string>() { "png", "jpg", "jpeg" }; //合法副檔名
+                    string response = folderFunction.FileProduceName(post.MachineImageObj, validFileNameEx);
+
+                    if (response == "Fail")
+                    {
+                        apiResult.Code = "2004"; //不合法的欄位
+                        apiResult.Message = _responseCodeHelper.GetResponseCodeString(apiResult.Code);
+                        return Ok(apiResult);
+                    }
+                    ImageName = response;
+                }
+
+                //判斷machineAdd資料夾是否存在
                 DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(_savePath, "machineAdd"));
                 if (!directoryInfo.Exists)
                 {
@@ -291,7 +326,8 @@ namespace ARManagement.Controllers
                         { "@CompanyId", CompanyId},
                         { "@MachineType", post.MachineType},
                         { "@ModelSeries", post.ModelSeries},
-                        { "@MachineModel", post.MachineModel},
+                        { "@MachineName", post.MachineName},
+                        { "@MachineImage", ImageName},
                         //{ "@Creator", myUser.UserId},
                         { "@Creator", 1},
                     };
@@ -316,12 +352,23 @@ namespace ARManagement.Controllers
                         { "@CompanyId", CompanyId},
                         { "@MachineType", post.MachineType},
                         { "@ModelSeries", post.ModelSeries},
-                        { "@MachineModel", post.MachineModel},
+                        { "@MachineName", post.MachineName},
                         //{ "@Updater", myUser.UserId},
                         { "@Updater", 1},
                         { "@UpdateTime", DateTime.Now}
                     };
 
+                    if (!string.IsNullOrEmpty(ImageName))
+                    {
+                        updateManchine_Dict.Add("@MachineImage", ImageName);
+                    }
+                    else
+                    {
+                        if ((bool)post.IsDeletedMachineImage)
+                        {
+                            updateManchine_Dict.Add("@MachineImage", "");
+                        }
+                    }
 
                     await _baseRepository.UpdateOneByCustomTable(updateManchine_Dict, "MachineAdd", "\"MachineAddId\" = @MachineAddId");
 
@@ -329,6 +376,12 @@ namespace ARManagement.Controllers
                 }
 
                 var fullPath = Path.Combine(_savePath, "machineAdd", MachineAddId.ToString()); //根目錄
+                if ((bool)post.IsDeletedMachineImage && !string.IsNullOrEmpty(machineinfo.MachineImage)) //刪除之前的圖片
+
+                if (!string.IsNullOrEmpty(ImageName)) //新增圖片
+                {
+                    folderFunction.SavePathFile(post.MachineImageObj, fullPath, ImageName);
+                 }
 
                 apiResult.Code = "0000";
                 apiResult.Message = _responseCodeHelper.GetResponseCodeString(apiResult.Code);
@@ -390,50 +443,14 @@ namespace ARManagement.Controllers
 
                 var machineWhere = $@"""MachineAddId"" = @MachineAddId";
 
-                #region 刪掉SOP表
-                //找出所有SOP
-                //var machineAlarmIds = machineAlarms.Select(x => x.MachineAlarmId).ToList();
-                //var sopWhere = $@"""MachineAlarmId"" = ANY (@MachineAlarmId)";
-                //var sops = await _baseRepository.GetAllAsync<SOP>("SOP", sopWhere, new { MachineAlarmId = machineAlarmIds });
-
-                ////刪除SOP
-                //var deleteSOP = $@"UPDATE public.""SOP"" SET
-                //                                ""Deleted"" = 1,
-                //                                ""Updater"" = @Updater,
-                //                                ""UpdateTime"" = @UpdateTime WHERE ""MachineAlarmId"" = ANY (@Ids)";
-                //await _baseRepository.ExecuteSql(deleteSOP, new { Updater = myUser.UserId, UpdateTime = DateTime.Now, Ids = machineAlarmIds });
-                #endregion
-
-                #region 刪掉MachineDevice表
-                //await _baseRepository.DeleteOne(machineInfo.MachineAddId, "MachineDevice", "\"MachineAddId\"", myUser.UserId);
-                #endregion
-
-                #region 刪掉MachineIOT表
-                ////找出所有MachineIOT
-                //var machineIOTs = await _baseRepository.GetAllAsync<IOT>("MachineIOT", machineWhere, new { MachineId = machineInfo.MachineId });
-
-                ////刪除MachineIOT
-                //await _baseRepository.DeleteOne(machineInfo.MachineId, "MachineIOT", "\"MachineId\"", myUser.UserId);
-                #endregion
-
-                #region 刪掉MachineIOTTopic
-                //var machineIOTIds = machineIOTs.Select(x => x.MachineIOTId).ToList();
-
-                //var deleteMachineIOTTopic = $@"UPDATE public.""MachineIOTTopic"" SET
-                //                                ""Deleted"" = 1,
-                //                                ""Updater"" = @Updater,
-                //                                ""UpdateTime"" = @UpdateTime WHERE ""MachineIOTId"" = ANY (@Ids)";
-                //await _baseRepository.ExecuteSql(deleteMachineIOTTopic, new { Updater = myUser.UserId, UpdateTime = DateTime.Now, Ids = machineIOTIds });
-                #endregion
-
                 #region 刪掉Machine底下所有子資料夾、檔案
-                //var machinePath = Path.Combine(_savePath,"machine", machineInfo.MachineId.ToString());
+                var machinePath = Path.Combine(_savePath, "machineAdd", machineInfo.MachineAddId.ToString());
 
-                //DirectoryInfo directoryInfo = new DirectoryInfo(machinePath);
-                //if (directoryInfo.Exists)
-                //{
-                //    directoryInfo.Delete(true);
-                //}
+                DirectoryInfo directoryInfo = new DirectoryInfo(machinePath);
+                if (directoryInfo.Exists)
+                {
+                    directoryInfo.Delete(true);
+                }
                 #endregion
 
                 apiResult.Code = "0000";
